a32249a8a308f32821a7a096a1dd5e36b4c0e34d
Ankur Mallick <108927188+AnkurMal@users.noreply.github.com>
2023-09-19 23:50:54 +0530

    Update platformer.c

--- 2D platformer (test)/platformer.c
+++ 2D platformer (test)/platformer.c
@@ -1,38 +1,45 @@
-#include "raylib.h"
+#include <raylib.h>
+#include <raymath.h>
 
+# define FRAMES_SPEED 10
 #define TILE_SIZE 50
-#define COLUMN 20
+#define COLUMN 25
 #define ROW 16
 
 Texture2D sky, grass, sun, dirt, player, coin;
-Sound jump;
+Vector2 cam_offset;
+Rectangle player_source_rect, player_dest_rect;
+Sound jump, coin_sound;
+Camera2D camera;
 
-int currentFrame = 0, framesCounter = 0, framesSpeed = 10;
-int dx, double_jump = 0, gravity = 0;
+int current_frame = 0, frames_counter = 0;
+int dx, dy = 0, double_jump = 0;
 
 //world map to draw
 int world_data[ROW][COLUMN] = {
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},              // 0 FOR AIR
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},              // 1 FOR DIRT                
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0},              // 2 FOR GRASS        
-                                {0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},                                             
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0}, 
-                                {0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                {0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0}, 
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0}, 
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-                                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}, 
-                                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},              // 0 FOR AIR
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},              // 1 FOR DIRT                
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0},              // 2 FOR GRASS        
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},              // 3 FOR COIN                         
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0}, 
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                                {0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                                {0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0}, 
+                                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}, 
+                                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+                                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, 
+                                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
                             };
 
-static void player_movement(Rectangle *player_source_rect, Rectangle *player_dest_rect, int speed);
-static void pause_movement(Rectangle *player_source_rect);
-static void check_collision(Rectangle *player_dest_rect);
+
+static void PlayerMovement(int speed);
+static void PauseMovement(void);
+static void CheckCollision(void);
+static void Draw(void);
 
 int main(void)
 {
@@ -49,10 +56,14 @@ int main(void)
     coin = LoadTexture("images/coin.png");
     
     player = LoadTexture("images/scarfy.png");
-    Rectangle player_source_rect = {((float)player.width/6)*2, 0.0f, (float)player.width/6, (float)player.height};
-    Rectangle player_dest_rect = {50.0f, (ROW-3)*TILE_SIZE, TILE_SIZE, TILE_SIZE};
+    player_source_rect = (Rectangle){((float)player.width/6)*2, 0.0f, (float)player.width/6, (float)player.height};
+    player_dest_rect = (Rectangle){0, 0, TILE_SIZE, TILE_SIZE};
+
+    cam_offset = (Vector2){ScreenWidth/2, (ROW-5)*TILE_SIZE};
+    camera = (Camera2D){cam_offset, (Vector2){player_dest_rect.x, 0}, 0, 1};
     
     jump = LoadSound("audio/jump.mp3");
+    coin_sound = LoadSound("audio/coin.mp3");
     
     SetTargetFPS(60);
     while(!WindowShouldClose())
@@ -60,51 +71,34 @@ int main(void)
         dx = 0;
 
         //movement of player (left or right)
-        if(IsKeyDown(KEY_RIGHT) && (player_dest_rect.x + player_dest_rect.width)<=ScreenWidth)
+        if(IsKeyDown(KEY_RIGHT) && (player_dest_rect.x+TILE_SIZE)<=(TILE_SIZE*COLUMN-cam_offset.x))
         {
            if (player_source_rect.width<0)
                 player_source_rect.width *= -1;
             
-           player_movement(&player_source_rect, &player_dest_rect, 5);
+           PlayerMovement(5);
         }
-        else if(IsKeyDown(KEY_LEFT) && (player_dest_rect.x)>=0)
+        else if(IsKeyDown(KEY_LEFT) && player_dest_rect.x>=-cam_offset.x)
         {
            if (player_source_rect.width>0)
                 player_source_rect.width *= -1;
             
-           player_movement(&player_source_rect, &player_dest_rect, -5);
+           PlayerMovement(-5);
         }
         if(IsKeyReleased(KEY_RIGHT) || IsKeyReleased(KEY_LEFT))
-            pause_movement(&player_source_rect);
+            PauseMovement();
 
         //jumping of player
         if(IsKeyPressed(KEY_SPACE) && (double_jump<2))
         {
             PlaySound(jump);
-            gravity = -15;
+            dy = -15;
             double_jump++;
-            pause_movement(&player_source_rect);
+            PauseMovement();
         }
 
-        check_collision(&player_dest_rect);
-        
-        //drawing on the screen
-        BeginDrawing();
-            DrawTexture(sky, 0, 0, WHITE);
-            DrawTexture(sun, TILE_SIZE*2, TILE_SIZE*2, WHITE);
-            for(int i=0;i<ROW;i++)
-                for(int j=0;j<COLUMN;j++)
-                {
-                    if (world_data[i][j]==1)
-                        DrawTexture(dirt, j*TILE_SIZE, i*TILE_SIZE, WHITE);
-                    else if(world_data[i][j]==2)
-                        DrawTexture(grass, j*TILE_SIZE, i*TILE_SIZE, WHITE);
-                    else if(world_data[i][j]==3)
-                        DrawTexture(coin, j*TILE_SIZE, i*TILE_SIZE+10, WHITE);
-                }
-            DrawTexturePro(player, player_source_rect, player_dest_rect, (Vector2){0, 0}, 0, WHITE);
-            DrawFPS(10, 10);
-        EndDrawing();
+        CheckCollision();
+        Draw(); 
     }
     //unloading textures/sounds and closing window
     UnloadTexture(sky);
@@ -114,6 +108,7 @@ int main(void)
     UnloadTexture(player);
     UnloadTexture(coin);
     UnloadSound(jump);
+    UnloadSound(coin_sound);
     
     CloseAudioDevice();
     CloseWindow();
@@ -122,32 +117,40 @@ int main(void)
 }
 
 //function for different animation and collision in x-axis
-void player_movement(Rectangle *player_source_rect, Rectangle *player_dest_rect, int speed)
+void PlayerMovement(int speed)
 {
-   framesCounter++;
-   if (framesCounter >= (60/framesSpeed) && (!double_jump))
+   frames_counter++;
+   if (frames_counter >= (60/FRAMES_SPEED) && (!double_jump))
     {
-        framesCounter = 0;
-        currentFrame++;
-        if (currentFrame > 5) 
-            currentFrame = 0;
-        player_source_rect->x = (float)currentFrame*(float)player.width/6;
+        frames_counter = 0;
+        current_frame++;
+        if (current_frame > 5) 
+            current_frame = 0;
+        player_source_rect.x = (float)current_frame*(float)player.width/6;
     }
     dx = speed;
 }   
 
 //function for pausing animation
-void pause_movement(Rectangle *player_source_rect)
+void PauseMovement(void)
 {
-    currentFrame = 2;
-    player_source_rect->x = (float)currentFrame*(float)player.width/6;
+    current_frame = 2;
+    player_source_rect.x = (float)current_frame*(float)player.width/6;
 }
 
 //checks for collision of the player with the required blocks/elements
-void check_collision(Rectangle *player_dest_rect)
+void CheckCollision(void)
 {
-    if(gravity<20)
-        gravity++;
+    Rectangle player_pos_screen = 
+                                    {
+                                        cam_offset.x+player_dest_rect.x,
+                                        cam_offset.y+player_dest_rect.y,
+                                        player_dest_rect.width,
+                                        player_dest_rect.height
+                                    };
+
+    if(dy<20)
+        dy++;
 
     for(int i=0;i<ROW;i++)
         for(int j=0;j<COLUMN;j++)
@@ -155,39 +158,68 @@ void check_collision(Rectangle *player_dest_rect)
             if(world_data[i][j]==2)
             {
                 //check for collision in y direction
-                if(CheckCollisionRecs(*player_dest_rect, (Rectangle){j*TILE_SIZE, i*TILE_SIZE-gravity, TILE_SIZE, TILE_SIZE}))
+                if(CheckCollisionRecs(player_pos_screen, (Rectangle){j*TILE_SIZE, i*TILE_SIZE-dy, TILE_SIZE, TILE_SIZE}))
                 {
                     //check if below the ground i.e. jumping
-                    if(gravity<0)
+                    if(dy<0)
                     {
-                        player_dest_rect->y = (i+1)*TILE_SIZE;
-                        gravity = 0;
+                        player_dest_rect.y = (i+1)*TILE_SIZE-cam_offset.y;
+                        dy = 0;
                     }
                     //check if above the ground i.e. falling
-                    else if(gravity>=0)
+                    else if(dy>=0)
                     {
-                        player_dest_rect->y = (i-1)*TILE_SIZE;
+                        player_dest_rect.y = (i-1)*TILE_SIZE-cam_offset.y;
                         double_jump = 0;
-                        gravity = 0;
+                        dy = 0;
                     }
                 }
                 //check for collision in x direction
-                if(CheckCollisionRecs(*player_dest_rect, (Rectangle){j*TILE_SIZE-dx, i*TILE_SIZE, TILE_SIZE, TILE_SIZE}))
+                if(CheckCollisionRecs(player_pos_screen, (Rectangle){j*TILE_SIZE-dx, i*TILE_SIZE, TILE_SIZE, TILE_SIZE}))
                     dx = 0;
             }
             if(world_data[i][j]==3)
-                if(CheckCollisionRecs(*player_dest_rect, (Rectangle){j*TILE_SIZE, i*TILE_SIZE+10, coin.width, coin.height}))
+                if(CheckCollisionRecs(player_pos_screen, (Rectangle){j*TILE_SIZE, i*TILE_SIZE+10, coin.width, coin.height}))
                 {
+                    PlaySound(coin_sound);
                     world_data[i][j]=0;
                 }
         }
 
-    //check if player is above window (y<0)
-    if(player_dest_rect->y<0)
+    //check if player is above window (y<0 in screen)
+    if(player_dest_rect.y<-cam_offset.y)
     {
-        player_dest_rect->y = 0;
-        gravity = 0;
+        player_dest_rect.y = -cam_offset.y;
+        dy = 0;
     }
-    player_dest_rect->y += gravity;
-    player_dest_rect->x += dx;
+    player_dest_rect.y += dy;
+    player_dest_rect.x += dx;
+}
+
+//drawing on the screen (and update camera)
+void Draw(void)
+{
+    if(player_dest_rect.x>0 && (player_dest_rect.x+cam_offset.x*2)<(TILE_SIZE*COLUMN))
+        camera.target = (Vector2){player_dest_rect.x, 0};
+
+    BeginDrawing();
+        ClearBackground(SKYBLUE);
+        BeginMode2D(camera);
+            DrawTextureV(sky, Vector2Negate(cam_offset), WHITE);
+            for(int i=0;i<ROW;i++)
+                for(int j=0;j<COLUMN;j++)
+                {
+                    Vector2 pos = {TILE_SIZE*j-cam_offset.x, TILE_SIZE*i-cam_offset.y};
+                    if (world_data[i][j]==1)
+                        DrawTextureV(dirt, pos, WHITE);
+                    else if(world_data[i][j]==2)
+                        DrawTextureV(grass, pos, WHITE);
+                    else if(world_data[i][j]==3)
+                        DrawTextureV(coin, pos, WHITE);
+                }
+            DrawTexturePro(player, player_source_rect, player_dest_rect, (Vector2){0, 0}, 0, WHITE);
+        EndMode2D();
+        DrawTexture(sun, TILE_SIZE*2, TILE_SIZE*2, WHITE);
+        DrawFPS(10, 10);
+    EndDrawing();
 }
